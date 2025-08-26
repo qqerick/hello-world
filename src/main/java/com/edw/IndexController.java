db35c56ed4f09db7aff626c5f886fb69237bed5d
qqerick <qqerick82@gmail.com>
2025-08-26 13:32:55 -0400

    Update IndexController.java

--- src/main/java/com/edw/IndexController.java
+++ src/main/java/com/edw/IndexController.java
@@ -18,7 +18,7 @@ public class IndexController {
     @GetMapping("/")
     public HashMap helloWorld() {
         return new HashMap<>(){{
-            put("Message", "Ejemplo con Spring...");
+            put("Message", "Ejemplo con Spring10...");
         }};
     }
 }
