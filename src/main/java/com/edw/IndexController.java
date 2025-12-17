613a81c009cc2fb86c30444a77e0d2b3d5117a0a
qqerick <qqerick82@gmail.com>
2025-12-17 14:11:06 -0400

    Update helloWorld message in IndexController

--- src/main/java/com/edw/IndexController.java
+++ src/main/java/com/edw/IndexController.java
@@ -18,7 +18,7 @@ public class IndexController {
     @GetMapping("/")
     public HashMap helloWorld() {
         return new HashMap<>(){{
-            put("Message", "Ejemplo con Spring y Pipeline...");
+            put("Message", "Ejemplo con Spring y Pipeline 111...");
         }};
     }
 }
