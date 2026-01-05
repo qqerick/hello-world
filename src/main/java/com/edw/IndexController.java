52a2502615367bb51031066daa88620c6a00c030
qqerick <qqerick82@gmail.com>
2026-01-05 17:40:26 -0400

    Update helloWorld message in IndexController

--- src/main/java/com/edw/IndexController.java
+++ src/main/java/com/edw/IndexController.java
@@ -18,7 +18,7 @@ public class IndexController {
     @GetMapping("/")
     public HashMap helloWorld() {
         return new HashMap<>(){{
-            put("Message", "Ejemplo con Nuevo kjjkfsjhksfa...");
+            put("Message", "Ejemplo con Nuevo Osvaldo...");
         }};
     }
 }
